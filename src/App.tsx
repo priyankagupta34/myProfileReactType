3ad7c06347a57a3c4767994f3544a27ea4f1c1c1
priyankagupta34 <34priyankagupta@gmail.com>
2022-01-18 14:06:59 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <div className="app">
+        <div className="flexDiv">
+          <div className="qwer1"></div>
+          <div className="qwer2">
+            Hi, This is Priyanka. I am fullstack developer. This is just a pet
+            project for me creating with Reactjs with Typescript and I know it
+            never actually works but having a profile is better then not having
+            one{" "}
+            <span role="img" aria-label="">
+              &#128517;
+            </span>
+            .
+            <br />
+            <br />
+            And display profile that you see on the left is actually me no
+            kidding{" "}
+            <span role="img" aria-label="">
+              &#128556;
+            </span>
+            .
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
